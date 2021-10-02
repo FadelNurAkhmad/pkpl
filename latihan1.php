82b560886f5870aa2fd01a543e665201bb35eac2
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2021-10-02 09:41:06 +0700

    Update latihan1.php

--- latihan1.php
+++ latihan1.php
@@ -1,15 +1,23 @@
 <?php 
 
-# perulangan akan dilakukan selama variabel $i bukan kelipatan 2
-for ($i=0; $i < 20; $i++){
+class Nilai {
+
+    function nilaiGenap(){
+        # perulangan akan dilakukan selama variabel $i bukan kelipatan 2
+        for ($i=0; $i < 20; $i++){
     
-    # ubah nilai $rand secara random dari angka 1-100
-    $rand = mt_rand(0, 100);
-    if($rand % 2 == 0) {
-        echo "Nilai Genap = {$rand} <br>";
+        # ubah nilai $rand secara random dari angka 1-100
+        $rand = mt_rand(0, 100);
+        if($rand % 2 == 0) {
+            echo "Nilai Genap = {$rand} <br>";
         
+            }
+        }
     }
 }
 
+$CetakNilai = new Nilai();
+
+echo $CetakNilai->nilaiGenap();
 
 ?>
